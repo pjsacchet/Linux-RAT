75bbe32b3c911cd342d41830a4ef42cf3ce645dd
pjsacchet <psacchet@proton.me>
2025-11-05 17:12:06 -0500

    Add base dirs for C2 and implant; add gitignore

---
+++ C2/RAT.py
@@ -0,0 +1,10 @@
+# Patrick Sacchet
+# This file is the main entrypoint for our C2 server functionality 
+
+
+
+def main():
+    return 
+
+if __name__ == '__main__':
+    main()
---
+++ Implant/Connect.c
@@ -0,0 +1,14 @@
+// Patrick Sacchet
+// This file handles startup functionality, to include:
+    // Open target port and listen for connection
+    // Establish connection w/ C2, configure callback parameters 
+    // Callout to C2 on set or random interval for tasking
+    // Execute tasking and send back results
+    // Continue until connection is terminated 
+
+#include <stdio.h>
+
+int main() {
+    printf("Test\n");
+    return 0;
+}
